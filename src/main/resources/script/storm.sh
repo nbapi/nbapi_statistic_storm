2bf16a5c01519c8e6f707e26a03cba197a44beeb
苏红涛 <hongtao.su@corp.elong.com>
2017-07-20 12:21:20 +0800

    storm 前后置脚本修改

--- src/main/resources/script/storm.sh
+++ src/main/resources/script/storm.sh
@@ -6,6 +6,6 @@ source .bash_profile
 cd /home/work/apache-storm-0.9.6/bin
 ./storm kill NBAPIStatisticTopology -w 30
 echo "storm kill NBAPIStatisticTopology finished."
-./storm jar nbapi_statistic_storm-jar-with-dependencies.jar com.elong.hotel.main.TopologyMainCluster
-echo "storm jar nbapi_statistic_storm-jar-with-dependencies.jar com.elong.hotel.main.TopologyMainCluster finished."
+./storm jar /home/work/nbapi_statistic_storm/nbapi_statistic_storm-jar-with-dependencies.jar com.elong.hotel.main.TopologyMainCluster
+echo "./storm jar /home/work/nbapi_statistic_storm/nbapi_statistic_storm-jar-with-dependencies.jar com.elong.hotel.main.TopologyMainCluster finished."
 exit $?
