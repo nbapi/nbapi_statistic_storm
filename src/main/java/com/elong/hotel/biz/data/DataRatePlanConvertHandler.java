236739ae15b500e16c8d4c250db1506f3627488f
苏红涛 <hongtao.su@corp.elong.com>
2017-10-20 14:44:40 +0800

    orderfrom转换为代理名字

--- src/main/java/com/elong/hotel/biz/data/DataRatePlanConvertHandler.java
+++ src/main/java/com/elong/hotel/biz/data/DataRatePlanConvertHandler.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.elong.hotel.biz.IBizHandler;
+import com.elong.hotel.biz.order.AgentNameClassifier;
 import com.elong.hotel.constant.Const;
 import com.elong.nb.DataRatePlanStatistic;
 
@@ -18,11 +19,12 @@ public class DataRatePlanConvertHandler implements IBizHandler {
 		List<JSONObject> rst = new LinkedList<JSONObject>();
 		DataRatePlanStatistic model = JSON.parseObject(jsonObject.toJSONString(), DataRatePlanStatistic.class);
 		
+		String proxyid = AgentNameClassifier.classify(model.getProxyId());
 		// rp数量
 		Map<String, Object> ratePlanMap = new HashMap<String, Object>();
 		ratePlanMap.put(Const.BUSINESS_TYPE, model.getBusiness_type());
 		ratePlanMap.put(Const.LOG_TIME, model.getLog_time());
-		ratePlanMap.put("proxyid", model.getProxyId());
+		ratePlanMap.put("proxyid", proxyid);
 		ratePlanMap.put("count", model.getRatePlanSize());
 		JSONObject jsonObj = new JSONObject(ratePlanMap);
 		rst.add(jsonObj);
@@ -30,7 +32,7 @@ public class DataRatePlanConvertHandler implements IBizHandler {
 		Map<String, Object> roomStatusMap = new HashMap<String, Object>();
 		roomStatusMap.put(Const.BUSINESS_TYPE, model.getBusiness_type());
 		roomStatusMap.put(Const.LOG_TIME, model.getLog_time());
-		roomStatusMap.put("proxyid", model.getProxyId());
+		roomStatusMap.put("proxyid", proxyid);
 		roomStatusMap.put("roomTrueCount", model.getRoomTrueSize());
 		roomStatusMap.put("roomFalseCount", model.getRoomFalseSize());
 		jsonObj = new JSONObject(roomStatusMap);
